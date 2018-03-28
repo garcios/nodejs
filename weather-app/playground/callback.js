13ea478e4debb374f3bc568990fe026c2ca61c49
Garcia <oscar.garcia@zuehlke.com>
2018-03-28 18:07:07 +0800

    playground

---
+++ weather-app/playground/callback.js
@@ -0,0 +1,21 @@
+console.log('Begin');
+
+var getUser = (id, callback) => {
+     var user ={
+         id:id,
+         name: 'Vikram'
+     };
+
+     setTimeout(()=>{
+        callback(user);
+     },3000);
+    
+};
+
+
+
+getUser(31, (user) =>{
+   console.log(user);
+});
+
+console.log('End');
