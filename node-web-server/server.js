0e984363bb96b45fcecd05fcf7892520ca16937f
Garcia <oscar.garcia@zuehlke.com>
2018-03-29 16:28:50 +0800

    few changes

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -57,7 +57,8 @@ app.get('/bad', (req,res) =>{
 });
 
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, ()=>{
-    console.log("Server is up on port 3000");
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
 });
